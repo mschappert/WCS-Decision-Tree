68fe780bf52cd351c48df47bc2b9cc3fec9ac111
mschappert <mkschap@gmail.com>
2024-02-14 13:51:09 -0500

    Create decision_tree.py
    Creation of decision tree python code

---
+++ decision_tree.py
@@ -0,0 +1,5 @@
+# Name: WCS Decision Tree
+# Desciption: This decision tree is for WCS which includes both marine and terrestrial data
+# Purpose: WCS will use this for its workers to help categorize, identify, and select data for the organization's projects
+# Date: 02/14/2024
+# Authors: Mikayla Schappert, Naoya Morishita, Claudia
