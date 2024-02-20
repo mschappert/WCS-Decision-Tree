ea2c69b9591c65570ca0c43c7d1e09238e1cee53
csbuszta <122855206+csbuszta@users.noreply.github.com>
2024-02-20 12:46:41 -0500

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -2,6 +2,6 @@
 # Desciption: This decision tree is for WCS which includes both marine and terrestrial data
 # Purpose: WCS will use this for its workers to help categorize, identify, and select data for the organization's projects
 # Date: 02/14/2024
-# Authors: Mikayla Schappert, Naoya Morishita, Claudia
+# Authors: Mikayla Schappert, Naoya Morishita, Claudia Buszta
 
 # Say something
