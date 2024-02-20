0707d30f2363a87c95a3e1f8530bfa9fa09b0075
mschappert <mkschap@gmail.com>
2024-02-20 16:38:35 -0500

    install wxpython

--- decision_tree.py
+++ decision_tree.py
@@ -1,7 +1,9 @@
 # Name: WCS Decision Tree
 # Desciption: This decision tree is for WCS which includes both marine and terrestrial data
 # Purpose: WCS will use this for its workers to help categorize, identify, and select data for the organization's projects
-# Date: 02/14/2024
+# Date Created: 02/14/2024
 # Authors: Mikayla Schappert, Naoya Morishita, Claudia
 
-# Say something
+# install wxpython
+# pip install wxPython==4.2.0
+
